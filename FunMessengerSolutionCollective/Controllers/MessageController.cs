72dce4337bcbfea3e5e03f67dc512991f2ae178d
Nissan Dookeran <nissan.dookeran@outlook.com>
2017-06-24 16:11:51 +1000

    Initial commit. Sample page, sample api, no data connector (yet)

---
+++ FunMessengerSolutionCollective/Controllers/MessageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FunMessengerSolutionCollective.Models;
+
+namespace FunMessengerSolutionCollective.Controllers
+{
+    public class MessageController : ApiController
+    {
+        Message[] messageHistory = new Message[]
+        {
+          new Message
+          {
+              Id = 1,
+              Sender = "Nissan Dookeran",
+              SenderImageUrl="https://pbs.twimg.com/profile_images/340937461/nissan.jpg",
+              Text="Hello world!",
+              DateTimeStamp = DateTime.Now
+          }
+        };
+
+        public IEnumerable<Message> GetMessageHistory()
+        {
+            return messageHistory;
+        }
+
+    }
+}
---
+++ FunMessengerSolutionCollective/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FunMessengerSolutionCollective.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Sender { get; set; }
+        public string SenderImageUrl { get; set; }
+        public DateTime DateTimeStamp { get; set; }
+    }
+}
